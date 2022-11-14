fca0072b56d3f5db08baba0a7c7b573c5be3eae4
Nicholas Velten <nicholas.velten@ligue.net>
2022-11-14 20:11:55 -0300

    📦 NEW: video service test

--- application/repositories/job_repository_test.go
+++ application/repositories/job_repository_test.go
@@ -27,11 +27,39 @@ func TestJobRepositoryDbInsert(t *testing.T) {
 	require.Nil(t, err)
 
 	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
 
 	j, err := repoJob.Find(job.ID)
 	require.NotEmpty(t, j.ID)
 	require.Nil(t, err)
 	require.Equal(t, j.ID, job.ID)
-	require.Equal(t, j.VideoId, Video.ID)
+	require.Equal(t, j.VideoID, video.ID)
+}
 
+func TestJobRepositoryDbUpdate(t *testing.T) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+	repo.Insert(video)
+
+	job, err := domain.NewJob("output_path", "Pending", video)
+	require.Nil(t, err)
+
+	repoJob := repositories.JobRepositoryDb{Db: db}
+	repoJob.Insert(job)
+
+	job.Status = "Complete"
+
+	repoJob.Update(job)
+
+	j, err := repoJob.Find(job.ID)
+	require.NotEmpty(t, j.ID)
+	require.Nil(t, err)
+	require.Equal(t, j.Status, job.Status)
 }
